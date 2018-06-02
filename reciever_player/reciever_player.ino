3c28d6e5344dd5ba6c16f26ecb8b0b7314239fcf
vishwasbg07 <39022018+vishwasbg07@users.noreply.github.com>
2018-06-02 20:54:45 +0530

    Add files via upload

---
+++ reciever_player/reciever_player.ino
@@ -0,0 +1,90 @@
+#include <DFRobotDFPlayerMini.h>
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
+
+SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
+DFRobotDFPlayerMini Player;
+int i = 0;
+char ch;
+void setup()
+{
+  mySoftwareSerial.begin(9600);
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println(F("Voice command mp3 player"));
+  Serial.println(F("Initializing mp3 player ... (May take 3~5 seconds)"));
+
+  if (!Player.begin(mySoftwareSerial))
+  {
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while (true);
+  }
+  
+  Serial.println(F("Player Mini Available"));
+  Player.volume(25);
+  delay(1000);
+}
+
+void loop()
+{
+
+  while (Serial.available())
+  {
+
+    ch = Serial.read();
+    Serial.println(ch);
+
+    switch (ch)
+    {
+      case 'a':
+        Player.play(2);
+        break;
+
+      case 'b':
+        Player.play(3);
+        break;
+
+      case 'c':
+        Player.play(4);
+        break;
+
+      case 'd':
+        Player.play(5);
+        break;
+
+      case 'z':
+        Player.play(6);
+        break;
+
+      case 'f':
+        Player.play(7);
+        break;
+
+      case 'g':
+        Player.play(8);
+        break;
+
+      case 'h':
+        Player.play(9);
+        break;
+
+      case 'i':
+        Player.play(1);
+        break;
+
+      case 'e':
+        Player.pause();
+        break;
+        
+      case 'j':
+        Player.pause();
+        break;
+    }
+    delay(3000);
+  }
+
+}
+
+
---
+++ transimitter_create_wifi/transimitter_create_wifi.ino
@@ -0,0 +1,104 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <SoftwareSerial.h>
+const char *ssid = "MASTER DEVICE";
+const char *password = "vishwas123";
+const char *code1 = "Attack a";
+const char *code2 = "Follow me b";
+const char *code3 = "Sucess c";
+const char *code4 = "Stop d";
+const char *code5 = "Alert z";
+const char *code6 = "Shoot Target f";
+const char *code7 = "At risk g";
+const char *code8 = "Knee down h";
+const char *code9 = "Cover up i";
+const char *code10 = "Reading j";
+
+SoftwareSerial ESP_ARD(2, SW_SERIAL_UNUSED_PIN); // RX, TX  |||| RX = ESP8266 = GPIO_2
+ESP8266WebServer server(80);
+void handleRoot()
+{
+
+  while (ESP_ARD.available())
+  {
+    char y = ESP_ARD.read();
+    if ( y == 97)
+    {
+      server.send(200, "text/html", code1);
+    }
+    else if ( y == 98)
+    {
+      server.send(200, "text/html", code2);
+    }
+    else if ( y == 99)
+    {
+      server.send(200, "text/html", code3);
+    }
+    else if ( y == 100)
+    {
+      server.send(200, "text/html", code4);
+
+    }
+    else if ( y == 101)
+    {
+      server.send(200, "text/html", code5);
+    }
+
+    else if ( y == 102)
+    {
+      server.send(200, "text/html", code6);
+
+    }
+    else if ( y == 103)
+    {
+
+      server.send(200, "text/html", code7);
+
+    }
+    else if ( y == 104)
+    {
+
+      server.send(200, "text/html", code8);
+
+    }
+    else if ( y == 105)
+    {
+
+      server.send(200, "text/html", code9);
+
+    }
+    else if ( y == 106)
+    {
+
+      server.send(200, "text/html", code10);
+
+    }
+
+  }
+}
+
+void setup()
+{
+  delay(1000);
+  Serial.begin(115200);
+  ESP_ARD.begin(115200);
+  Serial.println();
+  Serial.print("Configuring access point1q...");
+  WiFi.softAP(ssid, password);
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("HTTP server started");
+}
+void loop()
+{
+
+  handleRoot();
+  server.handleClient();
+
+}
+
+
